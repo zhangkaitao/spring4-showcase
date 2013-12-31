cec5a001ada9a7dc0fb5ae6cf51d17250c6d3e43
zhangkaitao <zhangkaitao0503@gmail.com>
2013-12-31 19:40:55 +0800

    first commit

--- spring4-others/src/test/java/com/sishuok/spring4/genericinject/GenericInjectTest.java
+++ spring4-others/src/test/java/com/sishuok/spring4/genericinject/GenericInjectTest.java
@@ -1,19 +1,26 @@
 package com.sishuok.spring4.genericinject;
 
 import com.sishuok.spring4.genericinject.component.BeanImpl1;
+import com.sishuok.spring4.genericinject.component.BeanImpl2;
 import com.sishuok.spring4.genericinject.component.BeanInteface;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * <p>User: Zhang Kaitao
@@ -25,18 +32,34 @@ import java.util.List;
 public class GenericInjectTest {
 
     @Autowired
+    @Qualifier("myList1")
+    private List<BeanInteface> myList1;
+
+    @Autowired
+    @Qualifier("myList2")
+    private List<BeanInteface> myList2;
+
+    @Autowired
     private List<BeanInteface> beanIntefaceList;
 
 
     @Test
     public void test() {
-        Assert.assertEquals(1, beanIntefaceList.size());
+        assertEquals(1, beanIntefaceList.size());
+        assertThat(beanIntefaceList, hasItem(isA(BeanImpl1.class)));
+
+        assertEquals(1, myList1.size());
+        assertThat(myList1, hasItem(isA(BeanImpl1.class)));
+
+        assertEquals(1, myList2.size());
+        assertThat(myList2, hasItem(isA(BeanImpl2.class)));
         //如果没有  public List<BeanInteface> customBeanIntefaceList() 则如下返回真
 //        Assert.assertEquals(2, beanIntefaceList.size());
     }
 
 
     @Configuration
+    @ImportResource("classpath:spring-genericInject.xml")
     @ComponentScan(basePackages = "com.sishuok.spring4.genericinject.component")
     static class GenericInjectConfig {
 
