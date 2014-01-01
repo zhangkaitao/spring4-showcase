bfa295fea0655149e29254144ea652945a8211f1
zhangkaitao <zhangkaitao0503@gmail.com>
2014-01-01 21:01:12 +0800

    first commit

---
+++ spring-cache/src/test/java/com/sishuok/spring/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.sishuok.spring.service;
+
+import com.sishuok.spring.AppConfig;
+import com.sishuok.spring.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-1
+ * <p>Version: 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    private Cache userCache;
+
+    @Before
+    public void setUp() {
+        userCache = cacheManager.getCache("user");
+    }
+
+
+    @Test
+    public void testCache() {
+        Long id = 1L;
+
+        User user = new User(id, "zhang", "zhang@gmail.com");
+        userService.save(user);
+
+        //一定要复制一个 否则cache了(因为同一个JVM测试的)
+        User user2 = new User(id, "zhang2", "zhang@gmail.com");
+        userService.conditionUpdate(user2);
+
+        userService.findById(id);
+        userService.findByUsername("zhang2");
+
+    }
+
+
+}
