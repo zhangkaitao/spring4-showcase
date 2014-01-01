736938a0125522ef62d7dae02b08613c7cf3f39d
zhangkaitao <zhangkaitao0503@gmail.com>
2014-01-01 20:59:53 +0800

    first commit

---
+++ spring-cache/src/main/java/com/sishuok/spring/AppConfig.java
@@ -0,0 +1,44 @@
+package com.sishuok.spring;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-1
+ * <p>Version: 1.0
+ */
+@Configuration
+@ComponentScan(basePackages = "com.sishuok.spring.service")
+@EnableCaching(proxyTargetClass = true)
+public class AppConfig implements CachingConfigurer {
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+
+        try {
+            net.sf.ehcache.CacheManager ehcacheCacheManager
+                    = new net.sf.ehcache.CacheManager(new ClassPathResource("ehcache.xml").getInputStream());
+
+            EhCacheCacheManager cacheCacheManager = new EhCacheCacheManager(ehcacheCacheManager);
+            return cacheCacheManager;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new SimpleKeyGenerator();
+    }
+}
---
+++ spring-cache/src/main/java/com/sishuok/spring/entity/User.java
@@ -0,0 +1,73 @@
+package com.sishuok.spring.entity;
+
+import java.io.Serializable;
+
+/**
+ * <p>User: Zhang Kaitao
+ * <p>Date: 14-1-1
+ * <p>Version: 1.0
+ */
+public class User implements Serializable {
+    private Long id;
+    private String username;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
